184cb7a36b758b30bda4f6494e21aebc17ef0a27
Chokchai213 <chokchai.bis@student.mahidol.edu>
2021-01-29 06:55:04 +0000

    change bubble sort

--- bubble.h
+++ bubble.h
@@ -8,26 +8,40 @@
 #ifndef bubble_h
 #define bubble_h
 #include "swap.h"
-void display(int value[], int N){
+void display(int value[], int N)
+{
     int i;
-    for(i=0;i<N;i++){
+    for(i=0; i<N; i++)
+    {
         printf("%5d",value[i]);
     }
     printf("\n");
 }
 
 
-void bubble(int a[], int N){
+void bubble(int a[], int N)
+{
     int i;
     int j;
-    
-    for(j=0;j<N-1;j++){
-        if(a[j]>a[j+1]) // decending
-            swap(&a[j],&a[j+1]);
-        display(a,N);
+    int sorted;
+    for (i = N - 1; i > 0; i--)
+    {
+        sorted = 0;
+        for (j = 0; j < i; j++)
+        {
+            if(a[j]>a[j+1]) // decending
+            {
+                swap(&a[j],&a[j+1]);
+                sorted = 1;
+                display(a,N);
+            }
+
+        }
+        if(sorted == 0)
+        {
+            break;
+        }
     }
-    
-    
 }
 
 #endif /* bubble_h */
